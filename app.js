d685a60ee071182e7276692ac197e321429b69a7
Scott Adams <scottadamsx@gmail.com>
2025-12-10 16:10:31 -0330

    Starting from scratch

--- app.js
+++
@@ -1,9 +0,0 @@
-import {Server, Table, Order, Item} from "./objects.js"
-
-const server = new Server("scotty")
-
-const table1 = new Table(1)
-const table2 = new Table(2)
-const table3 = new Table(3)
-
-server.section = [table1, table2, table3]
